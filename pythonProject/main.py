90bcdf6265824b1f82289f09c686cbe36992c012
Alberto Campini <alberto.campini@edu.unito.it>
2022-07-09 15:01:55 +0200

    bozza lettura in input e caricamento classi

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -2,19 +2,31 @@
 
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import math
+from datetime import datetime
+
 import xlrd as x
+import openpyxl as op
 import pandas as pd
+import Classes
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
 
-
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
 
-df = pd.read_excel('input/Calendario_ESAMI_Triennali__ESTATE_2022-2.xls')
-print(df)
+#init aule
+df = pd.read_excel('input/input modello.xlsx',skiprows=8, index_col=0, na_values=['string1', 'string2'],dtype={'Nome Aula': str, 'Date indisponibilità (Divise da ,)': datetime})
+df = df.reset_index()
+aule = []
+for index, row in df.iterrows():
+    if(row['Nome Aula']):
+        aule.append(Classes.ExamRoom(row['Nome Aula'],row['Date indisponibilità (Divise da ,)']))
+        print(row['Nome Aula'],row['Date indisponibilità (Divise da ,)'])
+
+print(aule[5].indisponibilita)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
